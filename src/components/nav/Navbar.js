b9d59296e2d7c41a7404783c35ab46012e90a616
Joseph_Tully <tully4school@gmail.com>
2020-01-13 01:55:59 -0500

    added new navbar

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./Navbar.css";
+import {
+	Navbar,
+	Nav,
+	NavDropdown,
+	Form,
+	FormControl,
+	Button
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Searchbar from "../Search/SearchBar";
 // import NavLink from "./Link.js";
 const url = "http://cocktail-recipes-tully4school.herokuapp.com/drinks/";
 
-class Navbar extends Component {
+class MainNavbar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -28,19 +36,59 @@ class Navbar extends Component {
 	}
 	render() {
 		return (
-			<ul>
-				<li>
-					<Link to='/'>Home</Link>
-					<Link to='/categories'>Categories</Link>
-					<Link to='/add'>Add A Drink</Link>
-					<Link to='/update'>Update A Drink</Link>
-					<Link to='/delete'>Remove A Drink</Link>
-					<Link to='/about'>About</Link>
-					<Link to='/Contact'>Contact</Link>
+			<Navbar bg='light' expand='lg'>
+				<Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='mr-auto'>
+						<Nav.Link href='#home'>Home</Nav.Link>
+						<Nav.Link tag={Link} to='/categories'>
+							Categories
+						</Nav.Link>
+						<NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+							<NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+							<NavDropdown.Item href='#action/3.2'>
+								Another action
+							</NavDropdown.Item>
+							<NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href='#action/3.4'>
+								Separated link
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
 					<Searchbar data={this.props.data} />
-				</li>
-			</ul>
+				</Navbar.Collapse>
+			</Navbar>
+			// <ul className='m-0'>
+			// 	<li>
+			// 		<li>
+			// 			<Link to='/'>Home</Link>
+			// 		</li>
+			// 		<li>
+			// 			<Link to='/categories'>Categories</Link>
+			// 		</li>
+			// 		<li>
+			// 			<Link to='/add'>Add A Drink</Link>
+			// 		</li>
+			// 		<li>
+			// 			<Link to='/update'>Update A Drink</Link>
+			// 		</li>
+			// 		<li>
+			// 			<Link to='/delete'>Remove A Drink</Link>
+			// 		</li>
+			// 		<li>
+			// 			<Link to='/about'>About</Link>
+			// 		</li>
+			// 		<li>
+			// 			<Link to='/Contact'>Contact</Link>
+			// 		</li>
+			// 		<li>
+			// 			<Searchbar data={this.props.data} />
+			// 		</li>
+			// 	</li>
+			// </ul>
 		);
 	}
 }
-export default Navbar;
+export default MainNavbar;
