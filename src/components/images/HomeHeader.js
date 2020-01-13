be4154b20f3cb41a75ca2592f75d09ad394ccb7c
Joseph_Tully <tully4school@gmail.com>
2020-01-12 23:18:58 -0500

    updated banner on homepage

--- src/components/images/HomeHeader.js
+++ src/components/images/HomeHeader.js
@@ -4,9 +4,9 @@ import "./Images.css";
 class HomeHeader extends Component {
 	render() {
 		return (
-			<div className='homeHeader'>
-				<h1>BoozeHound</h1>
-				<h5>Sniff Out Your Favorite Drink Recipes</h5>
+			<div className='homeHeader d-flex flex-column'>
+				<h1>Cocktail Creator</h1>
+				<h5>Find Your Favorite Drink Recipes</h5>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
