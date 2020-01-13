3be3a09b8db7f82073c35c28718db673a8b40ead
Joseph_Tully <tully4school@gmail.com>
2020-01-13 14:13:40 -0500

    attempting to show multiple images on slide mobile

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { Carousel } from "react-responsive-carousel";
+import Carousel from "react-bootstrap/Carousel";
+import "./Carousel.css";
+
+class HomeCarousel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: []
+		};
+		// this.props.data.push(this.state.data);
+		// console.log(this.state.data);
+	}
+
+	render() {
+		let newDrinks = this.props.data.slice(
+			Math.max(this.props.data.length - 20, 1)
+		);
+		console.log(newDrinks);
+
+		return (
+			<Carousel>
+				{newDrinks.map(item => (
+					<Carousel.Item>
+						<div
+							className='carousel-background d-none d-md-flex'
+							style={{
+								backgroundImage: `url(${item.drinkThumb})`
+							}}
+						></div>
+						<img
+							className='carousel-img d-flex align-items-center mt-md-3 w-sm-100'
+							src={item.drinkThumb}
+							alt='First slide'
+						/>
+						<Carousel.Caption>
+							<h3>{item.drinkName}</h3>
+							<p>{item.drinkIngredients}</p>
+						</Carousel.Caption>
+					</Carousel.Item>
+				))}
+			</Carousel>
+		);
+	}
+}
+export default HomeCarousel;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import "./Home.css";
 import HomeHeader from "../images/HomeHeader";
+import Carousel from "../Carousel/Carousel";
 
 class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: []
+		};
+	}
+
 	render() {
+		console.log(this.props.data);
 		return (
 			<div>
-				<HomeHeader />
+				<div className='row'>
+					<div className='col-md-12'>
+						<HomeHeader />
+					</div>
+				</div>
+				<div className='row my-5 carousel-row'>
+					<div className='d-flex col-md-12 flex-column justify-content-center'>
+						<h2 className='new-cocktails'>Recent Additions</h2>
+					</div>
+				</div>
+				<div className='row'>
+					<div className='col-md-12'>
+						<Carousel data={this.props.data} />
+					</div>
+				</div>
 			</div>
 		);
 	}
